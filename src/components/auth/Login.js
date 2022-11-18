a1096f37717816f9af064b3f5fe1c0b8960316ee
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-18 15:06:31 +0200

    Add possibility user to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,39 @@
+import { setPersistence, browserSessionPersistence, signInWithEmailAndPassword } from 'firebase/auth';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+
 export const Login = () => {
+    const { auth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email === '' || password === '') {
+            alert('Please fill all the fields');
+            return;
+        }
+
+        setPersistence(auth, browserSessionPersistence)
+            .then(() => {
+                signInWithEmailAndPassword(auth, email, password)
+                    .then(() => {
+                        navigate('/');
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            })
+    }
+
     return (
-        <form className='auth'>
+        <form className='auth' onSubmit={onLogin}>
             <h3>Login Here</h3>
             <label htmlFor="email">Username</label>
             <input type="text" placeholder="Email" id="email" name="email" />
